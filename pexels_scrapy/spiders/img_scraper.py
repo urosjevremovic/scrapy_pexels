d12268e9db998e160d1d748fa9a49633ee4ca8a6
urosjevremovic <jevremovic.uros91@gmail.com>
2018-07-19 17:42:46 +0200

    removed some .pyc files that ended up on git and added logic for user input so exact number of pages can be specified

--- pexels_scrapy/spiders/img_scraper.py
+++ pexels_scrapy/spiders/img_scraper.py
@@ -9,6 +9,7 @@ class ImgScraperSpider(scrapy.Spider):
     rotate_user_agent = True
     allowed_domains = ['pexels.com']
     start_urls = ['http://pexels.com/']
+    pages = int(input('Enter how many pages would you like to scrape. Each page contains 15 images. \nInput: '))
 
     def parse(self, response):
         urls = response.xpath('//div[contains(@class, "photos")]//article[contains(@class, "photo-item photo-item--overlay")]/a[contains(@class, "js-photo-link")]//@href').extract()
@@ -18,15 +19,13 @@ class ImgScraperSpider(scrapy.Spider):
             yield scrapy.Request(url=url, callback=self.parse_image)
 
         next_page_url = response.xpath('//div[contains(@class, "pagination")]/a[contains(@rel, "next")]//@href').extract_first()
-        if next_page_url:
+        if next_page_url and self.pages:
             next_page_url = response.urljoin(next_page_url)
+            self.pages -= 1
             yield scrapy.Request(url=next_page_url, callback=self.parse)
 
     def parse_image(self, response):
         item = PexelsScrapyItem()
-        # item['image_urls'] = response.css('picture.image-section__picture > image.image-section__image js-photo-zoom::attr(src)').extract_first()
         item['title'] = response.xpath('//img[contains(@class, "image-section__image js-photo-zoom")]//@alt').extract_first()
         item['image_urls'] = response.xpath('//img[contains(@class, "image-section__image js-photo-zoom")]//@src').extract()
-        # item['try_it'] = response.xpath('//img[contains(@class, "image-section__image js-photo-zoom")]//@src').extract()
-        print(item['image_urls'])
         yield item
